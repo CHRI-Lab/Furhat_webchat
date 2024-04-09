82f79e131156e7c778b6587898bbd2c26d030f3d
Zheng Zhuowen <zhengzhuowen@idea.edu.cn>
2024-04-09 14:22:20 +0800

    You could use WebScraper for testing

--- src/WebScraper.py
+++ src/WebScraper.py
@@ -6,20 +6,23 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 import openai
+from langchain_core.messages import HumanMessage
 
-openai.api_key = "PUT YOUR OPENAI KEY HERE"
+
+openai.api_key = "sk-xxx"
 os.environ["OPENAI_API_KEY"] = openai.api_key
-#chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
+# Before we get key from client, we could use our own key and use 3.5 for testing.
+chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.1)
 
 # Loader
-loader = WebBaseLoader("https://melbconnect.com.au/")
+loader = WebBaseLoader("https://melbconnect.com.au/about")
 data = loader.load()
 
 # TODO: Filter the data
 # TODO: Choose the best split method and the best chunk_size
 
 # Split the text into chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
 all_splits = text_splitter.split_documents(data)
 
 # Embedding and store the data into vector db
@@ -27,7 +30,56 @@ vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbedd
 
 # k is the number of chunks to retrieve
 retriever = vectorstore.as_retriever(k=4)
+question = "Tell me about the team in melbourne connect"
+docs = retriever.invoke(question)
 
-docs = retriever.invoke("Tell me about the latest news")
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-print("docs")
+SYSTEM_TEMPLATE = """
+Answer the user's questions based on the below web page information. 
+If the context doesn't contain any relevant information to the question, don't make something up and just say "I don't know":
+
+<web info>
+{context}
+</web info>
+"""
+
+question_answering_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            SYSTEM_TEMPLATE,
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
+
+from typing import Dict
+
+from langchain_core.runnables import RunnablePassthrough
+
+
+def parse_retriever_input(params: Dict):
+    return params["messages"][-1].content
+
+def format_docs(docs):
+    res = "\n\n".join(doc.page_content for doc in docs)
+    return res
+
+retrieval_chain = RunnablePassthrough.assign(
+    context=parse_retriever_input | retriever,
+).assign(
+    answer=document_chain,
+)
+
+ans = retrieval_chain.invoke(
+    {
+        "messages": [
+            HumanMessage(content=question)
+        ],
+    }
+)
+print(ans["answer"])
