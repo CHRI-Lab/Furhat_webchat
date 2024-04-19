49a5b8d91be7e644484adac518891b395a01791d
Jerry <623971174@qq.com>
2024-04-19 23:08:53 +1000

    This is a test

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 # To be developed
-print("Welcome to Koala")
+print("Welcome to QA-Koala")
 print("What would you like to do?")
+print("1. Add"
+      "2. Subtract"
+      "3. Multiply"
+      )
+def add(a, b):
+    return a + b
+add1 = 1
+add2 = 2
+print(add(add1, add2))
