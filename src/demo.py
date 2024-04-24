33c3558d29105012dc65c258f58c755a7a72df00
Boomm-shakalaka <61899807+Boomm-shakalaka@users.noreply.github.com>
2024-04-24 13:53:30 +1000

    fix slow_echo()

--- src/demo.py
+++ src/demo.py
@@ -4,7 +4,7 @@ import gradio as gr
 from src.chatbot import WebChatbot
 
 
-def slow_echo(message):
+def slow_echo(message,history):
     answer = chatbot.invoke(question=message)
     for i in range(len(answer)):
         time.sleep(0.01)
